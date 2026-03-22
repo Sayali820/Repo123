6d9b380b7fea8b485b6a663053989baa896b0b5f
Sayali820 <151982926+Sayali820@users.noreply.github.com>
2026-03-22 16:30:58 +0530

    calculator addded

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,71 +1,110 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Ignore;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CalculatorTest {
     private Calculator cal;
+
     @BeforeEach
-    void setObject()
+    void setup()
     {
         cal=new Calculator();
-
     }
     @Test
-    @DisplayName("addition test")
+    @Order(6)
     void addTest()
     {
-        assertEquals(5,cal.add(2,3));
-        assertNotEquals(6,cal.add(2,2));
+        assertEquals(8,cal.add(4, 4));
+        assertEquals(928,cal.add(563, 365));
+        assertNotEquals(45,cal.add(15, 25));
+        assertEquals(3,cal.add(0, 3));
         assertEquals(-5,cal.add(-10, 5));
-
+        assertEquals(-10,cal.add(-5, -5));
+        System.out.println("order 6");
     }
     @Test
+    @Order(5)
     void subTest()
     {
-        int res=cal.subtract(10, 5);
-        assertEquals(5,res);
-        assertTrue(res>0);
-
+        assertEquals(5,cal.subtract(10,5));
+        assertEquals(0,cal.subtract(5,5));
+        assertEquals(-9,cal.subtract(-5,4));
+        assertEquals(-6,cal.subtract(-10,-4));
+        assertEquals(5,cal.subtract(5,0));
+        assertEquals(-5,cal.subtract(0,5));
+        System.out.println("order 5");
 
     }
     @Test
+    @Order(4)
     void multiTest()
     {
-        assertEquals(6,cal.multiply(3, 2));
-        assertEquals(-8,cal.multiply(-4, 2));
-        assertEquals(9,cal.multiply(-3, -3));
+        assertEquals(16,cal.multiply(4,4));
+        assertEquals(-32,cal.multiply(-8,4));
+        assertEquals(72,cal.multiply(-9,-8));
+        assertEquals(1,cal.multiply(1,1));
+        assertEquals(0,cal.multiply(0,12));
+        System.out.println("order 4");
     }
     @Test
-    public void divideTest()
+    @Order(3)
+    void multidivide()
     {
+        assertEquals(-4,cal.divide(20,-5));
+        assertEquals(6,cal.divide(-36,-6));
+        assertEquals(0,cal.divide(0,19));
         assertThrows(ArithmeticException.class, ()->{
-            cal.divide(6,0);
+            cal.divide(9, 0);
         });
-        assertEquals(3,cal.divide(6, 2));
+        System.out.println("order 3");
     }
     @Test
-    void evenTest()
+    @Order(2)
+    @Disabled
+    void multiopTest()
     {
-        assertTrue(cal.isEven(-2));
-        assertFalse(cal.isEven(5));
-        assertTrue(cal.isEven(8),"write even no");
+        assertEquals(12,cal.add(5, 7));
+        assertEquals(2,cal.subtract(9,7));
+        assertEquals(40,cal.multiply(8,5));
+        assertEquals(3,cal.divide(9,3));
+        System.out.println("order 2");
     }
-    @Test
-    void multipleassertion()
+    @ParameterizedTest
+    @ValueSource(ints={2,4,6,8})
+    @Order(1)
+    void evenTest(int no)
     {
-        assertEquals(5,cal.add(5,0));
-        assertEquals(0,cal.multiply(5, 0));
-        assertEquals(-2, cal.divide(-10, 5));
+        assertTrue(cal.isEven(no));
+        System.out.println("order 1");
+    }
+    @AfterEach
+    void claenup()
+    {
+        cal=null;
     }
     @AfterAll
-    static void endmsg()
+    static void print()
     {
-        System.out.println("Test cases finished");
+        System.out.println("Finish testing");
+    }
+    @BeforeAll
+    static void printfirst()
+    {
+        System.out.println("Testing begin..");
     }
 
 
-    
+
 }
