356e91d6fb79d0d3d67cccf00762f4894174febe
Sayali820 <151982926+Sayali820@users.noreply.github.com>
2026-03-22 14:56:49 +0530

    grade code added

--- gradeTest.java
+++ gradeTest.java
@@ -1,81 +1,117 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-
-
+import org.junit.jupiter.api.TestMethodOrder;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class gradeTest {
     private grade gradu;
     @BeforeEach
-    void setobject()
+    void setup()
     {
         gradu=new grade();
-        System.out.println("setting the obj");
     }
     @Test
-    @DisplayName("Testing the code")
-    void TestforF()
+    @Order(9)
+    void testforF()
     {
-        assertEquals("F",gradu.grade(35));
-        System.out.println("F grade expected");
-        assertNotEquals("A",gradu.grade(0),"Error msg");
+        assertEquals("F",gradu.grade(38));
+        assertEquals("F",gradu.grade(20));
+        assertEquals("F",gradu.grade(0));
+        System.out.println("Order : 9");
     }
-    @RepeatedTest(2)
-    void repeatedTest()
-    {
-        System.out.println("Running");
-    }
-
     @Test
-    void TestforD()
+    @Order(8)
+    void testforD()
     {
+        assertEquals("D",gradu.grade(41));
         assertEquals("D",gradu.grade(45));
-        System.out.println("Grade is D");
-        assertNotEquals("A",gradu.grade(46));
-        System.out.println("grade should be D");
+        assertNotEquals("D",gradu.grade(0));
+        System.out.println("Order : 8");
     }
-    @Disabled("not raedy")
     @Test
-    void TestforC()
+    @Order(7)
+    void testforC()
     {
+        assertEquals("C",gradu.grade(56));
         assertEquals("C",gradu.grade(58));
-        assertNotEquals("A",gradu.grade(58));
+        assertNotEquals("C",gradu.grade(60));
+        System.out.println("Order : 7");
     }
     @Test
-    void TestforB()
+    @Order(6)
+    void testforB()
     {
-        assertEquals("B",gradu.grade(68));
-        assertNotEquals("A",gradu.grade(68));
+        assertEquals("B",gradu.grade(65));
+        assertNotEquals("B",gradu.grade(78));
+        System.out.println("Order : 6");
     }
     @Test
-    void TestforA()
+    @Order(5)
+    void testforA()
     {
-        assertEquals("A",gradu.grade(79));
-        assertNotEquals("C",gradu.grade(89));
+        assertEquals("A",gradu.grade(78));
+        assertNotEquals("A",gradu.grade(91));
+        System.out.println("Order : 5");
     }
     @Test
-    void TestforO()
+    @Order(4)
+    void testforA2()
     {
-        assertEquals("O",gradu.grade(99));
-        assertNotEquals("A",gradu.grade(98));
+        assertEquals("A+",gradu.grade(89));
+        assertNotEquals("A+",gradu.grade(50));
+        System.out.println("Order : 4");
     }
     @Test
-    void Testforneg()
+    @Order(3)
+    void testforO()
     {
-        assertThrows(IllegalArgumentException.class,()->{
-            gradu.grade(-11);
+        assertEquals("O",gradu.grade(98));
+        assertNotEquals("O",gradu.grade(89));
+        System.out.println("Order : 3");
+    }
+    @Test
+    @Order(2)
+    void negTest()
+    {
+        assertThrows(IllegalArgumentException.class, ()->{
+            gradu.grade(-67);
         });
-
+        System.out.println("Order : 2");
+    }
+    @Test
+    @Order(1)
+    void edgecase()
+    {
+        assertEquals("F",gradu.grade(0));
+        assertEquals("D",gradu.grade(40));
+        assertEquals("C",gradu.grade(50));
+        assertEquals("B",gradu.grade(60));
+        assertEquals("A",gradu.grade(70));
+        assertEquals("A+",gradu.grade(80));
+        assertEquals("O",gradu.grade(90));
+        System.out.println("Order : 1");
     }
     @AfterEach
-    void tearDown()
+    void cleanup()
     {
         gradu=null;
     }
-
+    @AfterAll
+    static void print()
+    {
+        System.out.println("testing finished..");
+    }
+    @BeforeAll
+    static void printfirst()
+    {
+        System.out.println("Testing begin..");
+    }
 
 }
+
